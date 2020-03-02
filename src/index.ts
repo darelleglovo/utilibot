cd78e3ca36f507bfc2ae8acc505c81b1625296e8
Darelle Glovo <darelle.glovo@pnp-hcl.com>
2020-03-03 07:55:52 +0800

    initial test run

--- src/index.ts
+++ src/index.ts
@@ -114,6 +114,7 @@ app.post('/webhook', (req, res) => {
                         } catch (e) {
                             console.log(e);
                             console.log('catchcatchcatchcatch')
+                            cache.del(senderPSID);
                             sendMessage(senderPSID, "I do not understand what you're saying. Please type \"help\" for the list of commands.")
                         }
 
