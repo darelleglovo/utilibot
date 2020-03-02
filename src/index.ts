53ff2a6e52ad10302b49ae932501064feee32d67
Darelle Glovo <darelle.glovo@pnp-hcl.com>
2020-03-03 07:10:01 +0800

    initial test run

--- src/index.ts
+++ src/index.ts
@@ -105,7 +105,7 @@ app.post('/webhook', (req, res) => {
                         // const response = pages[object].title + '\n' + pages[object].extract
                         // console.log(response);
                         for (let i = 0; i < contentBody.length; i++) {
-                            console.log(contentBody[i])
+                            console.log(i)
                             sendMessage(senderPSID, i.toString());
                         }
                         cache.del(senderPSID);
