aa499d2fdbefc12a1394e94c8379a018cd35ee6a
Darelle Glovo <darelle.glovo@pnp-hcl.com>
2020-03-03 16:34:51 +0800

    initial test run

--- src/variables.ts
+++ src/variables.ts
@@ -1,7 +1,7 @@
 export enum APIs {
     WIKIPEDIA = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&generator=prefixsearch&prop=extracts&exintro=1&explaintext=1&redirects=1&format=json&gpssearch=',
     CURRENCY_EXCHANGE = 'https://api.exchangeratesapi.io/latest',
-    OPENWEATHER = 'api.openweathermap.org/data/2.5/weather',
+    OPENWEATHER = 'https://api.openweathermap.org/data/2.5/weather',
 }
 
 export enum MESSAGES {
